55988ffa26222547e2f5407d3e8ce18f375320f9
AlcO <alcogr@gmail.com>
2019-12-31 14:47:51 +0200

    aesthetic

--- public/js/app.js
+++ public/js/app.js
@@ -49221,21 +49221,27 @@ var staticRenderFns = [
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "container" }, [
+    return _c("div", { staticClass: "container mx-auto" }, [
       _c("div", { staticClass: "row justify-content-center" }, [
-        _c("div", { staticClass: "col-md-8" }, [
-          _c("div", { staticClass: "card card-default" }, [
-            _c("div", { staticClass: "card-header" }, [
-              _vm._v("Example Component")
-            ]),
-            _vm._v(" "),
-            _c("div", { staticClass: "card-body" }, [
-              _vm._v(
-                "\n                    I'm an example component.\n                "
-              )
+        _c(
+          "div",
+          {
+            staticClass: "flex flex-col bg-white m-10 p-4 shadow-lg rounded-lg"
+          },
+          [
+            _c("div", { staticClass: "card card-default" }, [
+              _c("div", { staticClass: "border-b-2 pb-2" }, [
+                _vm._v("Example Component")
+              ]),
+              _vm._v(" "),
+              _c("div", { staticClass: "pt-2" }, [
+                _vm._v(
+                  "\n                    I'm an example component.\n                "
+                )
+              ])
             ])
-          ])
-        ])
+          ]
+        )
       ])
     ])
   }
--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="container">
+    <div class="container mx-auto">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="flex flex-col bg-white m-10 p-4 shadow-lg rounded-lg">
                 <div class="card card-default">
-                    <div class="card-header">Example Component</div>
+                    <div class="border-b-2 pb-2">Example Component</div>
 
-                    <div class="card-body">
+                    <div class="pt-2">
                         I'm an example component.
                     </div>
                 </div>
